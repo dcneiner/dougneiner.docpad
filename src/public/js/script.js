1c2de2e6b24847c981ef2ace3628ed09919d9eb7
Doug Neiner <doug@pixelgraphics.us>
2012-02-17 13:56:58 -0600

    Revised design, added some effects.

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -2,7 +2,23 @@
 
 */
 
+var $win = $( window ), 
+	$doc = $( document.documentElement ), 
+	threshold = 100,
+	old = 0;
 
+$win.bind( "scroll", function () {
+	var _new = $win.scrollTop();
 
-
-
+	if ( _new <= threshold && old > threshold ) {
+		$doc.removeClass( "scrolled" );
+		old = 0;
+		return;
+	}
+	
+	if ( _new > threshold && old <= threshold ) {
+		old = _new;
+		$doc.addClass( "scrolled" );
+		return;
+	}
+}).trigger( 'scroll' );
